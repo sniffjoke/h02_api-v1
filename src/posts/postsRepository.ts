1354a3bf0817926dd0481ba4b892fb84544398b4
sniffjoke <sniffjoke@gmail.com>
2024-07-25 01:46:05 +0200

    edit post-repository for tests

--- src/posts/postsRepository.ts
+++ src/posts/postsRepository.ts
@@ -19,8 +19,8 @@ export const postsRepository = {
             content: post.content,
             shortDescription: post.shortDescription,
             blogId: post.blogId,
-            // blogName: blogsRepository.findById(post.blogId)!.name,
-            blogName: post.blogName
+            blogName: blogsRepository.findById(post.blogId)!.name,
+            // blogName: post.blogName
         }
     },
     updatePost(post: any, id: string) {
